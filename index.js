c2fa7345ea8aa6e201073aee959cb617c46a4329
Valentina Angee <valen.angee@gmail.com>
2022-09-11 22:50:02 -0500

    Class1

---
+++ index.js
@@ -0,0 +1,9 @@
+let myNumber = 0;
+
+function handleClick() {
+  myNumber = myNumber + 1;
+  console.log(myNumber);
+  // const element = document.querySelector('#my-text');
+  // const newSize = Math.random() * 60 + 10;
+  // element.style.fontSize = `${newSize}px`;
+}
